4a7ab1b10d9de695a460c434041b377e5821fb10
Crazyskip <crazyskip73@gmail.com>
2022-03-09 15:01:27 +1000

    Fix casing issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Layout from "../components/Layout";
+import Layout from "../components/Layout.js";
 import { createGlobalStyle } from "styled-components";
 import { paleGrey, veryDarkNavy } from "../utils/colors";
 import { ProvideAuth } from "../hooks/useAuth";
